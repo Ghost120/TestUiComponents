48f4c44dbf45d09d6508d03c69d145d3198e8a13
Ghost120 <ghost120@mail.ru>
2017-07-06 07:33:55 +0300

    refactoring

--- src/test/java/test/pages/ResultsPage.java
+++ src/test/java/test/pages/ResultsPage.java
@@ -1,8 +1,11 @@
 package test.pages;
 
 import org.openqa.selenium.support.FindBy;
+//import ru.sbt.webdriver.elements.Button;
 import ru.sbt.webdriver.elements.Button;
 import ru.sbt.webdriver.elements.DateBox;
+import ru.sbt.webdriver.elements.DropDown;
+import ru.sbt.webdriver.elements.InputBox;
 import ru.sbt.webdriver.pages.BasePage;
 import ru.sbtqa.tag.pagefactory.PageFactory;
 import ru.sbtqa.tag.pagefactory.annotations.ActionTitle;
@@ -23,12 +26,20 @@ public class ResultsPage extends BasePage {
     private Button buttonCalculate;
 
     @ElementTitle("Дата открытия вклада")
-    @FindBy(xpath = "//div[2]//div[@class='kit-date-picker']")
+    @FindBy(xpath = "//div[2]/div/div[@class='kit-date-picker']")
     private DateBox openDate;
 
     @ElementTitle("Дата закрытия вклада")
-    @FindBy(xpath = "//div[3]//div[@class='kit-date-picker']")
-    private DateBox CloseDate;
+    @FindBy(xpath = "//div[3]/div/div[@class='kit-date-picker']")
+    private DateBox closeDate;
+
+    @FindBy(xpath = "//div[@class = 'kit-dropdown']")
+    @ElementTitle("Валюта вклада")
+    public DropDown valVklad;
+
+    @FindBy(xpath = "//div[@class='kit-masked-input']")
+    @ElementTitle("Сумма вклада")
+    public InputBox sumVlad;
 
     public ResultsPage() {
     }
